4ffcf2d0a3f7a4ae8e3ce6ce98f6582d70fba83e
jaemin <leejm_1994@naver.com>
2023-05-23 15:49:22 +0900

    Day1

---
+++ src/JavaPrac/Main.java
@@ -0,0 +1,40 @@
+package JavaPrac;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		  1.1) 정수타입
+		  1byte = byte 
+		  2byte =  short
+		  4byte = int 
+		  8byte = long
+		 
+		 1.2) 실수타입
+		 4byte = float
+		 8byte = double
+		 
+		 1.3) 논리타입
+		 1byte = boolean
+		 
+		 2.1)
+		 byte var = 200 x
+		 char var = 'AB' x
+		 char var = 65 x
+		 long var = 50000000000 
+		 float var = 3.14
+		 double var = 100.0 x
+		 String var = "나의 직업은 "개발자" 입니다." x
+		 boolean var = 0  x
+		 int v2 = le2 x
+		 float = 1e2f x
+		  
+		 */
+		
+		
+		
+	}
+
+}
